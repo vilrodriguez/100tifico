936b399e2e957bb715cc3ee918ea9d1a2f559681
vilrodriguez <vilmango@hotmail.com>
2020-11-27 01:34:29 -0500

    aa

---
+++ src/templates/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  const view = `
+    <header class="Header-main">
+      <div class="Header-logo">
+        <h1>
+          <a href="/"> 100tifi.co</a>
+        </h1>
+      </div>
+      <nav class="Header-nav">
+        <a href="#/about/">About</a>
+      </nav>
+    </header>
+  `;
+  return view;
+}
+export default Header;
