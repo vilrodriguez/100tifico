1a22e87c8e74d55002e1a2b2a67a0300eaa35ea3
vilrodriguez <vilmango@hotmail.com>
2020-12-01 22:23:22 -0500

    added gethash file and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,4 @@
-// import Header from '../templates/Header';
-// import Home from '../pages/Home';
-// import Character from '../pages/Character';
-// import Error404 from '../pages/Error404';
-
 // creating routes, creating an object indicating routes and where they'll be rendered
-
-// const routes = {
-//   '/': Home,
-//   id will be a dynamic number
-//   '/:id': Character,
-//   '/contact': 'Contact'
-// }
-
-// router manager, will show the elements, will take values from the borwser and see where the user is moving to
-
-// const router = async () => {
-//   const header = null || document.getElementById('header');
-//   const content = null || document.getElementById('content');
-
-//   header.innerHTML = await Header();
-// };
-
-// export default router;
 
 import Header from '../templates/Header';
 import Home from '../pages/Home';
